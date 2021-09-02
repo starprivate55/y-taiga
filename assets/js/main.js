556521dc890bb14f4f17800784ccc2671e1dc954
スター <83953883+starprivate55@users.noreply.github.com>
2021-09-03 00:44:10 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("menu-btn").onclick = function() {
+        this.classList.toggle('open');
+        this.classList.toggle('close');
+    }
+
+}
